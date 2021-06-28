662e7f6b5bb754c05f55fad22e3c5d9c5f4fcae3
RogiGral <igor.gralak99@gmail.com>
2021-06-28 15:02:49 +0200

    Gui
    Added gui for calculator

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,9 +10,9 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        Parent root = FXMLLoader.load(getClass().getResource("calculator.fxml"));
+        primaryStage.setTitle("CONVERSION CALCULATOR");
+        primaryStage.setScene(new Scene(root, 490, 275));
         primaryStage.show();
     }
 
